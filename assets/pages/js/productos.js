2a74fb0c082ebded5520f518e0623ff1fb33f560
Andre Chechile <chechile2005@gmail.com>
2023-11-17 19:28:55 -0300

    se mejoro el media screen

--- assets/pages/js/productos.js
+++ assets/pages/js/productos.js
@@ -20,15 +20,3 @@ const header = document.getElementById("header");
 const menuOpen = document.getElementById("menu-open");
 const menuClose = document.getElementById("menu-close");
 const navMenu = document.getElementById("nav-menu");
-//Menu responive
-if (menuOpen) {
-  menuOpen.addEventListener("click", () => {
-    navMenu.classList.add("header__nav--visible");
-  });
-}
-
-if (menuClose) {
-  menuClose.addEventListener("click", () => {
-    navMenu.classList.remove("header__nav--visible");
-  });
-}
